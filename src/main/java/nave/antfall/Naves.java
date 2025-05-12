87809c39c7aafb8479da0b6f4538f58954ad17a5
Antfall <animationsantfall@gmail.com>
2025-05-12 17:36:30 -0400

    Initial commit

---
+++ src/main/java/nave/antfall/Naves.java
@@ -0,0 +1,17 @@
+package nave.antfall;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Naves implements ModInitializer {
+	public static final String MOD_ID = "naves";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/nave/antfall/Navesclient.java
@@ -0,0 +1,10 @@
+package nave.antfall;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Navesclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
